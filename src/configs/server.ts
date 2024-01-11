11002861d349e7a3a4ff72f73adeace0b236e1b2
이진희 <speaker1403@gmail.com>
2024-01-11 22:56:33 +0900

    [dev] env를 포함하여 빌드

--- src/configs/server.ts
+++ src/configs/server.ts
@@ -1,2 +1,4 @@
 export const SERVER_URI =
-  process.env.REACT_APP_SERVER_URI || "http://localhost:8080/";
+  process.env.REACT_APP_SERVER_URI == undefined
+    ? "http://localhost:8080/"
+    : process.env.REACT_APP_SERVER_URI;
