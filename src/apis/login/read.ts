12123f2363d00270118b0a202ab6e9b31b00c191
이진희 <speaker1403@gmail.com>
2024-01-11 22:59:56 +0900

    [dev] kakao 오류 잡기

--- src/apis/login/read.ts
+++ src/apis/login/read.ts
@@ -3,6 +3,7 @@ import { SERVER_URI } from "../../configs/server";
 
 export const getOauthUrl = async (platform: string) => {
   try {
+    console.log(SERVER_URI + "social?type=kakao", "|");
     const res = await axios.get(SERVER_URI + "social?type=" + platform);
     return res.data.data;
   } catch (err) {
